95a0c5d5507f38377660e3f985c0bf9565faf6ef
mru24 <mru24@yahoo.com>
2023-12-01 12:30:34 +0000

    manual value enter try  -  falied

--- main.js
+++ main.js
@@ -3,25 +3,27 @@ var app = {
     init() {
         console.log("Input Type Number Buttons READY");
         this.inputNumber = this.jq('input[type="number"]');
-        this.inputNumber.length>0?this.initInputTypeNumber():'';
+        this.inputNumber.bind('keyup change click',(e)=>{
+            this.inputNumber.val(this.inputNumber.val());
+        });
+        this.inputNumber.length>0?this.initInputTypeNumber():'';        
     },
     initInputTypeNumber() {
-        this.inputNumber.wrap('<div class="ITN-container"></div>');
-        this.inputNumber.before('<span class="downITNbtn">-</span>');
-        this.inputNumber.after('<span class="upITNbtn">+</span>');
+        this.inputNumber
+            .wrap('<div class="ITN-container"></div>')
+            .before('<span class="downITNbtn">-</span>')
+            .after('<span class="upITNbtn">+</span>');
         let val = this.inputNumber.val();
         this.jq(document).on('click','.downITNbtn',(e)=>{
             if(val > 1) {
-                val --;
+                val--;
                 this.inputNumber.val(val);
-                console.log(val);
             }
         })
         this.jq(document).on('click','.upITNbtn',(e)=>{
             if(val > 0) {
-                val ++;
+                val++;
                 this.inputNumber.val(val);
-                console.log(val);
             }
         })
     },
